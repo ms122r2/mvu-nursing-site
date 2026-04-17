688dfd64e4b73e09de765fddc8a3b81b3fc5b4b7
LXS <mike.schlosser@boundlesslearning.com>
2026-04-17 21:42:09 +0000

    Clear for clean scaffold test

--- src/renderer/index.ts
+++
@@ -1,4 +0,0 @@
-export { PuckRenderer } from "./components/puck/PuckRenderer";
-export { MotionProvider } from "./components/puck/MotionProvider";
-export { puckConfig } from "./config";
-export type { Data } from "@puckeditor/core";
