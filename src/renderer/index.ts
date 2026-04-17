7e80ab015b327bf6796ff9d8e293401ea1fda6d7
ms122r2@gmail.com <ms122r2@gmail.com>
2026-04-17 21:17:30 +0000

    Publish Homepage v20

---
+++ src/renderer/index.ts
@@ -0,0 +1,4 @@
+export { PuckRenderer } from "./components/puck/PuckRenderer";
+export { MotionProvider } from "./components/puck/MotionProvider";
+export { puckConfig } from "./config";
+export type { Data } from "@puckeditor/core";
